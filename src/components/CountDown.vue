13644b53afdae0b72b55aefe362988ecc5f80a76
Skye García Blázquez <skye.garcia@feverup.com>
2025-08-22 15:01:17 +0200

    Add pulse animation to countdown

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -21,5 +21,20 @@ export default {
 .count-down {
   font-size: 1.5rem;
   text-align: center;
+  text-shadow: none;
+  animation: 3.5s linear 0s infinite alternate pulse;
+}
+
+@keyframes pulse {
+  0%,
+  100% {
+    text-shadow:
+      0 0 0.2rem var(--color-text),
+      0 0 0.4rem var(--color-text),
+      0 0 0.6rem var(--color-text);
+  }
+  50% {
+    text-shadow: none;
+  }
 }
 </style>
