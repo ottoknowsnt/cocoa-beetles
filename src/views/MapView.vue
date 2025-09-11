4923678ca939391a4bc88c18bd6b389a29d3f322
Skye García Blázquez <skye.garcia@feverup.com>
2025-09-12 00:52:04 +0200

    add style for map

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="map-container" class="map-container"></div>
+  <div class="map-container">
+    <div class="map-note">
+      <p>En cada una de las tres maravillas que he hecho solo para ti</p>
+      <p>un ojo de fantasma se oculta y se ve a la vez</p>
+    </div>
+    <div id="map" class="map"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -28,7 +34,7 @@ export default defineComponent({
   methods: {
     initMap() {
       this.map = new mapboxgl.Map({
-        container: 'map-container',
+        container: 'map',
         style: 'mapbox://styles/mapbox/standard',
         config: {
           basemap: {
@@ -108,8 +114,51 @@ export default defineComponent({
 
 <style scoped>
 .map-container {
-  width: 20rem;
-  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.map {
+  width: 100%;
+  height: 40rem;
+  clip-path: polygon(
+    0px calc(100% - 25px),
+    5px calc(100% - 25px),
+    5px calc(100% - 15px),
+    10px calc(100% - 15px),
+    10px calc(100% - 10px),
+    15px calc(100% - 10px),
+    15px calc(100% - 5px),
+    25px calc(100% - 5px),
+    25px 100%,
+    calc(100% - 25px) 100%,
+    calc(100% - 25px) calc(100% - 5px),
+    calc(100% - 15px) calc(100% - 5px),
+    calc(100% - 15px) calc(100% - 10px),
+    calc(100% - 10px) calc(100% - 10px),
+    calc(100% - 10px) calc(100% - 15px),
+    calc(100% - 5px) calc(100% - 15px),
+    calc(100% - 5px) calc(100% - 25px),
+    100% calc(100% - 25px),
+    100% 25px,
+    calc(100% - 5px) 25px,
+    calc(100% - 5px) 15px,
+    calc(100% - 10px) 15px,
+    calc(100% - 10px) 10px,
+    calc(100% - 15px) 10px,
+    calc(100% - 15px) 5px,
+    calc(100% - 25px) 5px,
+    calc(100% - 25px) 0px,
+    25px 0px,
+    25px 5px,
+    15px 5px,
+    15px 10px,
+    10px 10px,
+    10px 15px,
+    5px 15px,
+    5px 25px,
+    0px 25px
+  );
 }
 
 .marker {
@@ -118,5 +167,11 @@ export default defineComponent({
   border-radius: 50%;
   cursor: pointer;
   padding: 0;
+}
+
+.map-note {
+  font-size: 0.88rem;
+  text-transform: uppercase;
+  text-align: center;
 }
 </style>
