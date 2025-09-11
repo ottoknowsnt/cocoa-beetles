a2214155eac0a65b2b33c295a7990847b57d92eb
Skye García Blázquez <skye.garcia@feverup.com>
2025-09-12 01:42:20 +0200

    add style for challenge

--- src/components/SingleChallenge.vue
+++ src/components/SingleChallenge.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="challenge-text">
-    <img alt="Challenge Icon" :src="challengeIcon" width="125" height="125" />
-    <p>{{ challengeText }}</p>
+  <div class="challenge-container">
+    <img alt="Challenge Icon" :src="challengeIcon" class="challenge-icon" />
+    <p class="challenge-text" :class="{ 'challenge-text-bw': challengePassed }">
+      {{ challengeText }}
+    </p>
     <form @submit.prevent="validateChallenge">
       <input
         type="text"
+        class="challenge-input"
         v-model.trim="challengeResponse"
         placeholder="Introduce tu respuesta"
         :disabled="challengePassed"
       />
-      <p v-if="error">{{ error }}</p>
     </form>
-    <button :disabled="!challengePassed" @click="completeChallenge">Continuar</button>
+    <p class="challenge-error" v-if="error">{{ error }}</p>
+    <button class="challenge-button" :disabled="!challengePassed" @click="completeChallenge">
+      Continuar
+    </button>
   </div>
 </template>
 
@@ -34,6 +39,7 @@ export default {
       if (this.challengeResponse.toLowerCase() === this.challengeAnswer.toLowerCase()) {
         this.challengePassed = true
         this.error = ''
+        document.body.style.background = 'var(--color-background-bw)'
       } else {
         this.error = 'Respuesta incorrecta'
       }
@@ -42,6 +48,7 @@ export default {
       this.challengePassed = false
       this.challengeResponse = ''
       this.error = ''
+      document.body.style.background = 'var(--color-background)'
       this.$emit('completeChallenge')
     },
   },
@@ -49,23 +56,120 @@ export default {
 </script>
 
 <style scoped>
-.challenge-text {
-  font-size: 1.5rem;
-  text-align: center;
-  text-shadow: none;
-  animation: 3.5s linear 0s infinite alternate pulse;
+.challenge-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  place-items: center;
 }
 
-@keyframes pulse {
-  0%,
-  100% {
-    text-shadow:
-      0 0 0.2rem var(--color-text),
-      0 0 0.4rem var(--color-text),
-      0 0 0.6rem var(--color-text);
-  }
-  50% {
-    text-shadow: none;
-  }
+.challenge-text {
+  font-size: 1.25rem;
+  text-transform: uppercase;
+  text-align: center;
+  transition: all 1s ease-in-out;
+}
+
+.challenge-text-bw {
+  color: white;
+  text-shadow: 0.15rem 0.15rem 0 black;
+}
+
+.challenge-error {
+  color: red;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.challenge-icon {
+  width: 8rem;
+  height: 8rem;
+}
+
+.challenge-input {
+  font-family: inherit;
+  width: 19rem;
+  height: 3rem;
+  text-align: center;
+  color: var(--coraline-tunnel-dark-purple);
+  background-color: var(--coraline-tunnel-light-purple);
+  border: none;
+  clip-path: polygon(
+    0px calc(100% - 8px),
+    4px calc(100% - 8px),
+    4px calc(100% - 4px),
+    8px calc(100% - 4px),
+    8px 100%,
+    calc(100% - 8px) 100%,
+    calc(100% - 8px) calc(100% - 4px),
+    calc(100% - 4px) calc(100% - 4px),
+    calc(100% - 4px) calc(100% - 8px),
+    100% calc(100% - 8px),
+    100% 8px,
+    calc(100% - 4px) 8px,
+    calc(100% - 4px) 4px,
+    calc(100% - 8px) 4px,
+    calc(100% - 8px) 0px,
+    8px 0px,
+    8px 4px,
+    4px 4px,
+    4px 8px,
+    0px 8px
+  );
+  transition: all 1s ease-in-out;
+}
+
+.challenge-input:focus {
+  outline: none;
+}
+
+.challenge-input:disabled {
+  background-color: white;
+  color: black;
+  opacity: 0.8;
+}
+
+.challenge-input::placeholder {
+  color: var(--coraline-tunnel-dark-purple);
+}
+
+.challenge-button {
+  font-family: inherit;
+  width: 19rem;
+  height: 3rem;
+  text-align: center;
+  color: black;
+  background-color: white;
+  border: none;
+  clip-path: polygon(
+    0px calc(100% - 8px),
+    4px calc(100% - 8px),
+    4px calc(100% - 4px),
+    8px calc(100% - 4px),
+    8px 100%,
+    calc(100% - 8px) 100%,
+    calc(100% - 8px) calc(100% - 4px),
+    calc(100% - 4px) calc(100% - 4px),
+    calc(100% - 4px) calc(100% - 8px),
+    100% calc(100% - 8px),
+    100% 8px,
+    calc(100% - 4px) 8px,
+    calc(100% - 4px) 4px,
+    calc(100% - 8px) 4px,
+    calc(100% - 8px) 0px,
+    8px 0px,
+    8px 4px,
+    4px 4px,
+    4px 8px,
+    0px 8px
+  );
+  transition: all 1s ease-in-out;
+}
+
+.challenge-button:disabled {
+  color: var(--coraline-tunnel-dark-purple);
+  background-color: var(--coraline-tunnel-light-purple);
+  opacity: 0.8;
 }
 </style>
